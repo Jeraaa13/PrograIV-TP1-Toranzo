bb266de3a0e4daa1f4de47ea3c71bc8f37c1e073
Jeraaa13 <gerardo.m.toranzo@gmail.com>
2026-05-17 16:31:49 -0300

    agregar keys al env

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,7 @@
 export const environment = {
-  production: false,
-  supabaseUrl: '',
-  supabaseKey: '',
-  unsplashKey: '',
+  production: true,
+  supabaseUrl: 'https://acksthslyrococdrhfel.supabase.co',
+  supabaseKey:
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFja3N0aHNseXJvY29jZHJoZmVsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzY4ODEwOTYsImV4cCI6MjA5MjQ1NzA5Nn0.7t7qLq8emuxkLegz3hDYaGlYK0P7qqdYBpplzJ4vNqs',
+  unsplashKey: 'S3dGhze6_iyVjr8th5WGyZkCDpvgPQX-dsFQFgZmfyU',
 };
